6411d2a24bf4edf751c463d627a7c84ede0b927f
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-02-18 00:04:51 +0200

    add contact actions

---
+++ index.js
@@ -0,0 +1,46 @@
+import { program } from "commander";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact
+} from './contacts.js';
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+program.parse();
+
+const options = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contactList = await listContacts();
+      console.table(contactList);
+      break;
+
+    case "get":
+      const targetContact = await getContactById(id);
+      console.log(targetContact);
+      break;
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removedContact = await removeContact(id);
+      console.log(removedContact)
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
